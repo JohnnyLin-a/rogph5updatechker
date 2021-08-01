ed280f58fb3c6b6f6c05ef24ca4cef9fc9591ef3
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2021-07-31 23:07:15 -0400

    Add selenium function to fetch latest version

---
+++ main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+def fetchLatestVersionDetails():
+    driver = webdriver.Chrome("./chromedriver/chromedriver.exe")
+
+    driver.get("https://rog.asus.com/ca-en/phones/rog-phone-5-model/helpdesk_bios")
+    
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "productSupportDriverBIOSBox")))
+        versionEntries = driver.find_elements_by_class_name("productSupportDriverBIOSBox")
+        versionDetails = versionEntries[0].text.split("\n")
+        return [versionDetails[0].strip(), versionDetails[1].split(" ")[0].strip()]
+        # version = versionDetails[1].split(" ")[0].strip()
+    except TimeoutException:
+        print("Waiting too long and aborted")
+        exit(1)
+    finally:
+        driver.quit()
+
+
+# Start of script execution
+arr = fetchLatestVersionDetails()
+print(arr)
+
